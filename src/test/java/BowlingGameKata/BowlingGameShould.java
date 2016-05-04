963f5b296510589dee0edbaeb354c9f3edc2cdfe
Mani Sarkar <mani@codurance.com>
2016-05-04 21:36:58 +0100

    Fixing test inputs to be more realistic with two balls thrown in every frame.

--- src/test/java/BowlingGameKata/BowlingGameShould.java
+++ src/test/java/BowlingGameKata/BowlingGameShould.java
@@ -10,21 +10,21 @@ public class BowlingGameShould {
 
   @Test public void
   contain_ten_frames_when_game_completed() {
-    int frames = playGame("1|2|3|4|5|6|7|8|9|X");
+    int frames = playGame("1-|2-|3-|4-|5-|6-|7-|8-|9-|X");
 
     assertThat(frames, is(10));
   }
 
   @Test public void
   contain_less_than_ten_frames_when_game_has_not_finished() {
-    int frames = playGame("1|2|3");
+    int frames = playGame("1-|2-|3-");
 
     assertThat(frames, is(lessThan(10)));
   }
 
   @Test public void
   contains_7_frames_when_player_has_played_7_turns() {
-    int frames = playGame("1|2|3|4|5|6|7");
+    int frames = playGame("-1|-2|-3|-4|-5|-6|-7");
 
     assertThat(frames, is(7));
   }
