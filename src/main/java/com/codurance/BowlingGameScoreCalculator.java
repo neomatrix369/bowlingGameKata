0252d820dd416e35b5107c8360a1dd8ccb905c95
Mani Sarkar <mani@codurance.com>
2016-05-08 23:47:30 +0100

    Refactorings and extracting constants.

--- src/main/java/com/codurance/BowlingGameScoreCalculator.java
+++ src/main/java/com/codurance/BowlingGameScoreCalculator.java
@@ -4,9 +4,15 @@ import static java.lang.Integer.parseInt;
 
 public class BowlingGameScoreCalculator {
 
+  private static final String STRIKE = "X";
+  private static final int STRIKE_SCORE = 10;
+
+  private static final String MISS = "-";
+  private static final String FRAME_SEPARATOR = "\\|";
+
   public int evaluate(String frames, int forFrame) {
     int internalForFrame = forFrame - 1;
-    String splitFrames[] = frames.split("\\|");
+    String splitFrames[] = frames.split(FRAME_SEPARATOR);
 
     return evaluateFrame(splitFrames, internalForFrame);
   }
@@ -23,17 +29,24 @@ public class BowlingGameScoreCalculator {
     int framesScore = 0;
     String eachFrame = splitFrames[forFrame];
 
-    if (eachFrame.equals("X")) {
-      framesScore += 10 + evaluateFrame(splitFrames, forFrame + 1);
+    if (eachFrame.equals(STRIKE)) {
+      framesScore += evaluateFrameWithStrike(splitFrames, forFrame);
     } else {
-      framesScore += evaluateTheThrow(eachFrame.substring(0, 1)) + evaluateTheThrow(eachFrame.substring(1));
+      framesScore += evaluateOtherThrowsInFrame(eachFrame);
     }
 
     return framesScore;
   }
 
-  private int evaluateTheThrow(String aThrow) {
-    if (aThrow.equals("-")) {
+  private int evaluateFrameWithStrike(String[] splitFrames, int forFrame) {
+    final int nextFrame = forFrame + 1;
+    return STRIKE_SCORE + evaluateFrame(splitFrames, nextFrame);
+  }
+
+  private int evaluateOtherThrowsInFrame(String eachFrame) {return evaluateThisThrow(eachFrame.substring(0, 1)) + evaluateThisThrow(eachFrame.substring(1));}
+
+  private int evaluateThisThrow(String aThrow) {
+    if (aThrow.equals(MISS)) {
       return 0;
     }
     return parseInt(aThrow);
