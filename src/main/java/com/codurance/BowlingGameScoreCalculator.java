52fd2bab1bb25ef43b5c4ee594c22a452abdb526
Mani Sarkar <mani@codurance.com>
2016-05-08 23:35:05 +0100

    Now can calculate score for frame with a strike followed by two throws.

--- src/main/java/com/codurance/BowlingGameScoreCalculator.java
+++ src/main/java/com/codurance/BowlingGameScoreCalculator.java
@@ -1,8 +1,32 @@
 package com.codurance;
 
+import static java.lang.Integer.parseInt;
+
 public class BowlingGameScoreCalculator {
 
-  public int evaluate(String frames) {
-    return 10;
+  public int evaluate(String frames, int forFrame) {
+    forFrame = forFrame - 1;
+    String splitFrames[] = frames.split("\\|");
+
+    return evaluateFrame(splitFrames, forFrame);
+  }
+
+  private int evaluateFrame(String[] splitFrames, int forFrame) {
+    if (forFrame >= splitFrames.length) return 0;
+
+    return calculateFrameScore(splitFrames, forFrame);
+  }
+
+  private int calculateFrameScore(String[] splitFrames, int forFrame) {
+    int framesScore = 0;
+    String eachFrame = splitFrames[forFrame];
+
+    if (eachFrame.equals("X")) {
+      framesScore += 10 + evaluateFrame(splitFrames, forFrame + 1);
+    } else {
+      framesScore += parseInt(eachFrame.substring(0, 1)) + parseInt(eachFrame.substring(1));
+    }
+
+    return framesScore;
   }
 }
--- src/test/java/com/codurance/BowlingGameScoreCalculatorShould.java
+++ src/test/java/com/codurance/BowlingGameScoreCalculatorShould.java
@@ -7,12 +7,20 @@ import org.junit.Test;
 
 public class BowlingGameScoreCalculatorShould {
   @Test public void
-  calculate_frame_score_for_a_strike() {
+  calculate_frame_score_for_a_strike_in_the_first_frame() {
     BowlingGameScoreCalculator bowlingGameScoreCalculator = new BowlingGameScoreCalculator();
 
-    int frameScore = bowlingGameScoreCalculator.evaluate("X|");
+    int frameScore = bowlingGameScoreCalculator.evaluate("X|", 1);
 
     assertThat(frameScore, is(10));
-  } 
+  }
 
+  @Test public void
+  calculate_frame_score_for_a_strike_and_two_throws_for_the_first_frame() {
+    BowlingGameScoreCalculator bowlingGameScoreCalculator = new BowlingGameScoreCalculator();
+
+    int frameScore = bowlingGameScoreCalculator.evaluate("X|64", 1);
+
+    assertThat(frameScore, is(10 + 6 + 4));
+  }
 }
