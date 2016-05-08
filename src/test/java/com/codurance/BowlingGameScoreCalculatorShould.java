e0bc96af01ea6f17ee53348226e9bacf0316f34e
Mani Sarkar <mani@codurance.com>
2016-05-09 00:05:11 +0100

    Some more duplications removed in the test

--- src/test/java/com/codurance/BowlingGameScoreCalculatorShould.java
+++ src/test/java/com/codurance/BowlingGameScoreCalculatorShould.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 public class BowlingGameScoreCalculatorShould {
 
   private BowlingGameScoreCalculator bowlingGameScoreCalculator;
+
   @Before
   public void initialise() {
     bowlingGameScoreCalculator = new BowlingGameScoreCalculator();
@@ -16,29 +17,23 @@ public class BowlingGameScoreCalculatorShould {
 
   @Test public void
   calculate_frame_score_for_a_strike_in_the_first_frame() {
-    int frameScore = bowlingGameScoreCalculator.evaluate("X|", 1);
-
-    assertThat(frameScore, is(10));
+    assertThat(getFrameScoreFor("X|"), is(10));
   }
 
   @Test public void
   calculate_frame_score_for_a_strike_and_two_throws_for_the_first_frame() {
-    int frameScore = bowlingGameScoreCalculator.evaluate("X|64", 1);
-
-    assertThat(frameScore, is(10 + 6 + 4));
+    assertThat(getFrameScoreFor("X|64"), is(10 + 6 + 4));
   }
   
   @Test public void
   calculate_frame_score_for_9_pins_hit_and_a_miss_in_the_first_frame() {
-    int frameScore = bowlingGameScoreCalculator.evaluate("9-|", 1);
-
-    assertThat(frameScore, is(9));
+    assertThat(getFrameScoreFor("9-|"), is(9));
   }
 
   @Test public void
   calculate_frame_score_for_5_pins_hit_and_a_spare_in_the_first_frame() {
-    int frameScore = bowlingGameScoreCalculator.evaluate("5/|", 1);
-
-    assertThat(frameScore, is(10));
+    assertThat(getFrameScoreFor("5/|"), is(10));
   }
+
+  private int getFrameScoreFor(String frames) {return bowlingGameScoreCalculator.evaluate(frames, 1);}
 }
