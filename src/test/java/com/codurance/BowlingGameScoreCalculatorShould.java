dfa1e26d99ed0c6a4f41fda044bd7b1e67d1b042
Mani Sarkar <mani@codurance.com>
2016-05-09 19:59:27 +0100

    Refactored the BowlingGameScoreCalculatorShould into parameterised tests

--- src/test/java/com/codurance/BowlingGameScoreCalculatorShould.java
+++ src/test/java/com/codurance/BowlingGameScoreCalculatorShould.java
@@ -2,42 +2,50 @@ package com.codurance;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static java.util.Arrays.asList;
+
+import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class BowlingGameScoreCalculatorShould {
 
+  private final String inputFrames;
+  private final int expectedFrameScore;
+
   private BowlingGameScoreCalculator bowlingGameScoreCalculator;
+
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return asList(
+        new Object[][] {
+            {"X|",    10},
+            {"X|64",  10 + 6 + 4},
+            {"9-|",   9},
+            {"5/|",   10},
+            {"5/|23", 12}
+        }
+    );
+  }
 
   @Before
   public void initialise() {
     bowlingGameScoreCalculator = new BowlingGameScoreCalculator();
   }
 
-  @Test public void
-  calculate_frame_score_for_a_strike_in_the_first_frame() {
-    assertThat(getFrameScoreFor("X|"), is(10));
+  public BowlingGameScoreCalculatorShould(String inputFrames, int expectedFrameScore) {
+    this.inputFrames = inputFrames;
+    this.expectedFrameScore = expectedFrameScore;
   }
 
-  @Test public void
-  calculate_frame_score_for_a_strike_and_two_throws_for_the_first_frame() {
-    assertThat(getFrameScoreFor("X|64"), is(10 + 6 + 4));
-  }
-  
-  @Test public void
-  calculate_frame_score_for_9_pins_hit_and_a_miss_in_the_first_frame() {
-    assertThat(getFrameScoreFor("9-|"), is(9));
-  }
-
-  @Test public void
-  calculate_frame_score_for_5_pins_hit_and_a_spare_in_the_first_frame() {
-    assertThat(getFrameScoreFor("5/|"), is(10));
-  }
-
-  @Test public void
-  calculate_frame_score_for_5_pins_hit_and_a_spare_in_the_first_frame_followed_by_one_of_the_two_throws_in_the_next_frame() {
-    assertThat(getFrameScoreFor("5/|23"), is(12));
+  @Test
+  public void
+  calculate_frame_score_from_the_input_frames() {
+    assertThat(getFrameScoreFor(inputFrames), is(expectedFrameScore));
   }
 
   private int getFrameScoreFor(String frames) {return bowlingGameScoreCalculator.evaluate(frames, 1);}
