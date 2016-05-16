609267b3bf2ef18bf9d1e8fb45c0df3ede2b20e0
Mani Sarkar <mani@codurance.com>
2016-05-16 20:16:21 +0100

    Minor refactoring of implementation code

--- src/main/java/com/codurance/BowlingGameScoreCalculator.java
+++ src/main/java/com/codurance/BowlingGameScoreCalculator.java
@@ -4,15 +4,19 @@ import static java.lang.Integer.parseInt;
 
 public class BowlingGameScoreCalculator {
 
+  private static final String MISSED_THROW = "-";
+  private static final String SPARE_THROW = "/";
+  private static final String SPLIT_DELIMETER = "";
+
   public int evaluate(String turns) {
-    String[] turnsSplit = turns.split("");
+    String[] turnsSplit = turns.split(SPLIT_DELIMETER);
 
     int score = 0;
     int turnScore;
     for (String turn: turnsSplit) {
-      if (turn.equals("-")) {
+      if (turn.equals(MISSED_THROW)) {
         turnScore = 0;
-      } else if (turn.equals("/")) {
+      } else if (turn.equals(SPARE_THROW)) {
         return 10;
       } else {
         turnScore = parseInt(turn);
