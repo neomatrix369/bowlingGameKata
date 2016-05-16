9052ea37632140784f50902c19dd38ed74370699
Mani Sarkar <mani@codurance.com>
2016-05-16 20:50:51 +0100

    Calculate scores for pins knocked and missed

--- src/main/java/com/codurance/BowlingGameScoreCalculator.java
+++ src/main/java/com/codurance/BowlingGameScoreCalculator.java
@@ -1,3 +1,27 @@
 package com.codurance;
 
-public class BowlingGameScoreCalculator {}
+import static java.lang.Integer.parseInt;
+
+public class BowlingGameScoreCalculator {
+
+  private static final String TURNS_SEPARATOR = "";
+  private static final String MISSED_THROW = "-";
+
+  public int evaluate(String turns) {
+    String[] splitTurns = turns.split(TURNS_SEPARATOR);
+
+    int score = 0;
+    for (String turn: splitTurns) {
+      score += getScoreForTheTurn(turn);
+    }
+
+    return score;
+  }
+
+  private int getScoreForTheTurn(String turn) {
+    if (turn.equals(MISSED_THROW)) {
+        return 0;
+    }
+    return parseInt(turn);
+  }
+}
--- src/test/java/com/codurance/BowlingGameScoreCalculatorShould.java
+++ src/test/java/com/codurance/BowlingGameScoreCalculatorShould.java
@@ -1,10 +1,13 @@
 package com.codurance;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static java.util.Arrays.asList;
 
 import java.util.Collection;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -14,10 +17,21 @@ public class BowlingGameScoreCalculatorShould {
 
   private BowlingGameScoreCalculator bowlingGameScoreCalculator;
 
+  private String turns;
+  private int score;
+
+  public BowlingGameScoreCalculatorShould(String turns, int score) {
+    this.turns = turns;
+    this.score = score;
+  }
+
   @Parameters( name = "{index}: {0} = {1}" )
   public static Collection<Object[]> data() {
     return asList(
         new Object[][] {
+            {"11111111111111111111", 20},
+            {"12131415161718191213", 59},
+            {"-2-3-4-5-6-7-8-9-2-3", 49}
         }
     );
   }
@@ -27,4 +41,8 @@ public class BowlingGameScoreCalculatorShould {
     bowlingGameScoreCalculator = new BowlingGameScoreCalculator();
   }
 
+  @Test public void
+  calculate_throws_in_each_turn_for_all_the_frames() {
+      assertThat(bowlingGameScoreCalculator.evaluate(turns), is(score));
+  } 
 }
