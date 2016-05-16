3ceff5c69de02d696b504523a1b59190967ae93e
Mani Sarkar <mani@codurance.com>
2016-05-16 21:42:23 +0100

    Calculates score for spares and misses for all 10 frames

--- src/main/java/com/codurance/BowlingGameScoreCalculator.java
+++ src/main/java/com/codurance/BowlingGameScoreCalculator.java
@@ -5,23 +5,53 @@ import static java.lang.Integer.parseInt;
 public class BowlingGameScoreCalculator {
 
   private static final String TURNS_SEPARATOR = "";
+
   private static final String MISSED_THROW = "-";
+  private static final String SPARE_THROW = "/";
+  private static final String NO_THROW = "";
 
   public int evaluate(String turns) {
     String[] splitTurns = turns.split(TURNS_SEPARATOR);
 
     int score = 0;
-    for (String turn: splitTurns) {
-      score += getScoreForTheTurn(turn);
+    int index = 0;
+    while (index < turns.length()) {
+      String turn = splitTurns[index];
+      String nextTurn = "";
+      if (index < turns.length() - 1) {
+        nextTurn = splitTurns[index + 1];
+        if (turn.equals(SPARE_THROW) &&
+            index == 19) {
+          nextTurn = "";
+        }
+      }
+
+      score += getScoreForTheThrow(turn, nextTurn);
+      index++;
     }
 
     return score;
   }
 
-  private int getScoreForTheTurn(String turn) {
-    if (turn.equals(MISSED_THROW)) {
-        return 0;
+  private int getScoreForTheThrow(String thisThrow, String nextThrow) {
+    if (thisThrow.equals(NO_THROW) ||
+        thisThrow.equals(MISSED_THROW) ||
+        nextThrow.equals(SPARE_THROW)) {
+      return 0;
     }
-    return parseInt(turn);
+
+    if (thisThrow.equals(SPARE_THROW)) {
+      return 10 + parseToNumber(nextThrow);
+    }
+
+    return parseToNumber(thisThrow);
+  }
+
+  private int parseToNumber(String nextTurn) {
+    if (nextTurn.equals(NO_THROW)) {
+      return 0;
+    }
+
+    return parseInt(nextTurn);
   }
 }
